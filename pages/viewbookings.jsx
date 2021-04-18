f989489e5a4bafa8d3b7b7fc41b03b158a104e23
Sujith <Sujith.Dachepally@student.uts.edu.au>
2021-04-18 13:22:21 +0530

    added cancel booking functionality

--- pages/viewbookings.jsx
+++ pages/viewbookings.jsx
@@ -6,6 +6,9 @@ import { useRouter } from 'next/router'
 const viewbookings = ({bookingusers}) => {
     
     const [bid, setId] = useState(false)
+    const [Confirm, setConfirm] = useState(false)
+    const [show, setShow] = useState(false)
+
     const list = new Array()
     const list1 = new Array()
     var today_date = new Date()
@@ -25,11 +28,32 @@ const viewbookings = ({bookingusers}) => {
             if(t_day<=user.date)
                 list1.push(user)
     }
+    const Cancel = (event) => {
+        event.preventDefault()
+            setConfirm(true)
+    }
+    const noCancel = () => {
+        setConfirm(false)
+    }
+
+    const yesCancel = async () => {
+        setConfirm(false)
+        const res = await fetch(
+            `http://localhost:5000/bookings/${bid}`,
+            {
+                method: 'DELETE'
+            }
+        )
+        if (res.status == 200)
+            router.push('/custhome')
+        else setShow(true)
+    }
 
     return (
         <div>
             <Link href="/custhome"><a>Back</a></Link>
             <h3>Previous Bookings</h3>
+            {show ? <p>Something went wrong or no values were entered</p> : null}
             <table>
                 <thead>
                     <tr><td>Date</td><td>Time</td><td>Table</td><td>Number Of People</td></tr>
@@ -42,6 +66,17 @@ const viewbookings = ({bookingusers}) => {
             ))}
             </tbody>
             <h3>Current Bookings</h3>
+            {Confirm ?  
+                <div>
+                    <div>
+                        <div>
+                            <p>Are you sure you want to delete this member?</p>
+                            <button onClick={yesCancel}>Yes</button>
+                            <button onClick={noCancel}>No</button>
+                        </div>
+                    </div>
+                </div>
+                : null}
             <table>
                 <thead>
                     <tr><td>Date</td><td>Time</td><td>Table</td><td>Number Of People</td></tr>
@@ -50,7 +85,8 @@ const viewbookings = ({bookingusers}) => {
             <tbody>
             {list1.map((buser) => (
                         <tr key={buser.name}><td>{buser.date}</td>
-                            <td>{buser.time}</td><td>{buser.table}</td><td>{buser.numberofpeople}</td></tr>
+                            <td>{buser.time}</td><td>{buser.table}</td><td>{buser.numberofpeople}</td>
+                            <td><button onClick={Cancel}>Cancel Booking</button></td></tr>
             ))}
             </tbody>
         </div>
